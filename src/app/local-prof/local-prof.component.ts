b1da2780d567b278d7b1810eed0843c113dab814
Goosa2025 <gus21002@byui.edu>
2023-04-13 09:02:23 -0600

    Update

---
+++ src/app/local-prof/local-prof.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-local-prof',
+  templateUrl: './local-prof.component.html',
+  styleUrls: ['./local-prof.component.css']
+})
+export class LocalProfComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
