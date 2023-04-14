d9619fa30bfd6a48ee9627179d5e929d519ec92e
Goosa2025 <gus21002@byui.edu>
2023-04-14 10:45:13 -0600

    component added

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,13 +6,23 @@ import { AppComponent } from './app.component';
 import { MainComponent } from './main/main.component';
 import { NavComponent } from './nav/nav.component';
 import { LocalProfComponent } from './local-prof/local-prof.component';
+import { MapFindComponent } from './map-find/map-find.component';
+import { HomeComponent } from './home/home.component';
+import { FooterComponent } from './footer/footer.component';
+import { LoginComponent } from './login/login.component';
+import { AboutComponent } from './about/about.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     MainComponent,
     NavComponent,
-    LocalProfComponent
+    LocalProfComponent,
+    MapFindComponent,
+    HomeComponent,
+    FooterComponent,
+    LoginComponent,
+    AboutComponent
   ],
   imports: [
     BrowserModule,
---
+++ src/app/map-find/map-find.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-map-find',
+  templateUrl: './map-find.component.html',
+  styleUrls: ['./map-find.component.css']
+})
+export class MapFindComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
