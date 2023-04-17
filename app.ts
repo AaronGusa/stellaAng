4ca64101185b28d2486f15669629b6425827dd5a
Goosa2025 <gus21002@byui.edu>
2023-04-17 13:38:28 -0600

    bug hunt 6

---
+++ app.ts
@@ -0,0 +1,29 @@
+//dependencies
+const express = require('express');
+const path = require('path');
+const http = require('http');
+const bodyParser = require('body-parser');
+const cookieParser = require('cookie-parser');
+const cors = require('cors');
+
+//calls
+const app = express();
+const port = process.env.PORT || 3000;
+
+//import routing file
+app 
+    .use(express.json())
+    .use(cors())
+    .use('/', require('./backend/routes'))
+    
+
+const start = async () => {
+    app.listen(port, () => {
+        if (app.listen) {
+            console.log(`Running on port ${port}`);
+        } else {
+            console.log(`error - not connected`);
+        }
+    })
+}
+start();
